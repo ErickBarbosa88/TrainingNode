a5bc959d4917ac57f0ee3789a62b9c2faa5eb925
ErickBarbosa88 <barbosaerick88@gmail.com>
2024-05-17 12:22:43 -0300

    fixnamefile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useUser } from '../context/contextsUser';
+import { useUser } from '../context/ContextsUser.tsx';
 
 const Profile = () => {
   const { userName } = useUser(); 
