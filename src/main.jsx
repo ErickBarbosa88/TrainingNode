6f4a86a4f00ada1d080e38b03bf32cd2ebbb266c
ErickBarbosa88 <barbosaerick88@gmail.com>
2024-05-17 12:31:54 -0300

    remove context

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { UserProvider } from './context/ContextsUser.tsx';
 import Profile from "./components/profile";
 import Login from "./components/login";
 import Register from "./components/cadastro";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
     <BrowserRouter>
       <Routes>
         <Route index element={<Login />} />
@@ -16,6 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
-    </UserProvider>
   </React.StrictMode>
 );
